228ace1a651a3787f9ad3bf7a9776e86959110ea
Patrick Smith <patrick@burntcaramel.com>
2017-12-06 11:41:01 +1100

    Init Express server

---
+++ api/server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const server = express()
+
+// Middleware Plugins
+server.use(bodyParser.json()) // Allows me to have JSON uploads (POST/PUT/PATCH)
+
+// Routes
+
+// Start the server
+server.listen(7000, (error) => {
+  if (error) {
+    console.error('Error starting', error)
+  }
+  else {
+    console.log('Server started at http://localhost:7000/')
+  }
+})
