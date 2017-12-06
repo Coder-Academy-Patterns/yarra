17825159f794ec0538d57f50819ea0c33cedbc43
Patrick Smith <patrick@burntcaramel.com>
2017-12-06 22:27:14 +1100

    Add cors to api

--- api/server.js
+++ api/server.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 const authMiddleware = require('./middleware/auth')
 
 const server = express()
 
 // Middleware Plugins
 server.use(bodyParser.json()) // Allows me to have JSON uploads (POST/PUT/PATCH)
+server.use(cors()) // Allow cross-origin requests, i.e. from React front-end
 server.use(authMiddleware.initialize) // Kick passport off
 
 // Routes
