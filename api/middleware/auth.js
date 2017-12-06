48f3da135d91c87fa9c7779bffea78893baad6dc
Patrick Smith <patrick@burntcaramel.com>
2017-12-06 15:33:14 +1100

    Add JWT authorization

--- api/middleware/auth.js
+++ api/middleware/auth.js
@@ -1,5 +1,6 @@
 const passport = require('passport')
 const JWT = require('jsonwebtoken')
+const PassportJwt = require('passport-jwt')
 const User = require('../models/User')
 
 const jwtSecret = '.]y#rg9C43evhEsy'
@@ -30,6 +31,38 @@ function register(req, res, next) {
   })
 }
 
+passport.use(new PassportJwt.Strategy(
+  // Options
+  {
+    // Where will the JWT be passed in the HTTP request?
+    // e.g. Authorization: Bearer eyJhbGc…
+    jwtFromRequest: PassportJwt.ExtractJwt.fromAuthHeaderAsBearerToken(),
+    // What is the secret
+    secretOrKey: jwtSecret,
+    // What algorithm(s) was used to sign it?
+    algorithms: [jwtAlgorithm]
+  },
+  // When we have a verified token
+  (payload, done) => {
+    // Find the real user from our database using the `id` in the JWT
+    User.findById(payload.sub)
+      .then((user) => {
+        // If user was found with this id
+        if (user) {
+          done(null, user)
+        }
+        // If not user was found
+        else {
+          done(null, false)
+        }
+      })
+      .catch((error) => {
+        // If there was a failure
+        done(error, false)
+      })
+  }
+))
+
 function signJWTForUser(req, res) {
   // Get the user (either just signed in or signed up)
   const user = req.user
@@ -56,5 +89,6 @@ module.exports = {
   initialize: passport.initialize(),
   register,
   signIn: passport.authenticate('local', { session: false }),
+  requireJWT: passport.authenticate('jwt', { session: false }),
   signJWTForUser
 }
--- api/routes/products.js
+++ api/routes/products.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const Product = require('../models/Product')
+const authMiddleware = require('../middleware/auth')
 
 const router = new express.Router()
 
-router.get('/products', (req, res) => {
+router.get('/products', authMiddleware.requireJWT, (req, res) => {
   Product.find()
     .then((products) => {
       res.json(products)
