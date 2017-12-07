2179eb272c5068c00c35a94448421bbc76d44937
Patrick Smith <patrick@burntcaramel.com>
2017-12-07 15:54:42 +1100

    Add update product route to API

--- api/routes/products.js
+++ api/routes/products.js
@@ -4,6 +4,7 @@ const authMiddleware = require('../middleware/auth')
 
 const router = new express.Router()
 
+// Read list
 router.get('/products', authMiddleware.requireJWT, (req, res) => {
   Product.find()
     .then((products) => {
@@ -14,10 +15,30 @@ router.get('/products', authMiddleware.requireJWT, (req, res) => {
     })
 })
 
+// Create
 router.post('/products', authMiddleware.requireJWT, (req, res) => {
   Product.create(req.body)
     .then((product) => {
       res.status(201).json(product)
+    })
+    .catch((error) => {
+      res.status(400).json({ error })
+    })
+})
+
+// Update
+router.put('/products/:id', authMiddleware.requireJWT, (req, res) => {
+  const { id } = req.params
+  Product.findByIdAndUpdate(id, req.body, { new: true, runValidators: true })
+    .then((product) => {
+      if (product) {
+        res.json(product)
+      }
+      else {
+        res.status(404).json({
+          error: new Error(`Product with id '${id}' not found`)
+        })
+      }
     })
     .catch((error) => {
       res.status(400).json({ error })
