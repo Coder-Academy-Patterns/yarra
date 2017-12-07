3847354216b5135aaa37131198f23b3ec485d466
Patrick Smith <patrick@burntcaramel.com>
2017-12-07 11:03:31 +1100

    Add submission of sign form

--- web/src/App.js
+++ web/src/App.js
@@ -3,12 +3,18 @@ import './App.css';
 import SignInForm from './components/SignInForm'
 
 class App extends Component {
+  onSignIn = ({ email, password }) => {
+    console.log('App received', { email, password })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Yarra</h1>
         <h2 className='mb-3'>Now Delivering: Shipping trillions of new products</h2>
-        <SignInForm />
+        <SignInForm
+          onSignIn={ this.onSignIn }
+        />
       </div>
     );
   }
--- web/src/components/SignInForm.js
+++ web/src/components/SignInForm.js
@@ -1,10 +1,24 @@
 import React from 'react'
 
 function SignInForm({
-
+  onSignIn
 }) {
   return (
-    <form>
+    <form
+      onSubmit={ (event) => {
+        // Prevent old-school form submission
+        event.preventDefault()
+        
+        const form = event.target
+        const elements = form.elements // Allows looking up fields using their 'name' attributes
+        // Get entered values from fields
+        const email = elements.email.value
+        const password = elements.password.value
+
+        // Pass this information along to the parent component
+        onSignIn({ email, password })
+      } }
+    >
       <label
         className='mb-2'
       >
