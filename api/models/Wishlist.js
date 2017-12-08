00e7e9719c04b3ed91f77448537bfdad299ce21a
Patrick Smith <patrick@burntcaramel.com>
2017-12-08 12:33:14 +1100

    API: Add add to wishlist route

--- api/models/Wishlist.js
+++ api/models/Wishlist.js
@@ -5,7 +5,7 @@ const wishlistSchema = new Schema({
   // Belongs to user
   user: { type: Schema.ObjectId, ref: 'User', unique: true },
   // Has many products
-  product: [{ type: Schema.ObjectId, ref: 'Product' }]
+  products: [{ type: Schema.ObjectId, ref: 'Product' }]
 })
 
 const Wishlist = mongoose.model('Wishlist2', wishlistSchema)
--- api/routes/wishlist.js
+++ api/routes/wishlist.js
@@ -21,4 +21,26 @@ router.get('/wishlist', requireJWT, (req, res) => {
     })
 })
 
+// Add product to wishlist
+router.post('/wishlist/products/:productID', requireJWT, (req, res) => {
+  const { productID } = req.params
+  Wishlist.findOneAndUpdate(
+    // Find the wishlist for the signed in user
+    { user: req.user },
+    // Make these changes
+    // $addToSet: https://docs.mongodb.com/manual/reference/operator/update/addToSet/
+    { $addToSet: { products: productID } },
+    // Options when updating
+    // upsert: Update if wishlist exists, insert (create wishlist) if not
+    // new: Give us the updated wishlist
+    { upsert: true, new: true, runValidators: true }
+  )
+    .then((wishlist) => {
+      res.json({ products: wishlist.products })
+    })
+    .catch((error) => {
+      res.status(400).json({ error })
+    })
+})
+
 module.exports = router
