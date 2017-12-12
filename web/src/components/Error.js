68b730f0f12bcea4d1887504a1a106a17e67ba2a
Patrick Smith <patrick@burntcaramel.com>
2017-12-12 15:05:30 +1100

    Improve error messages

--- web/src/components/Error.js
+++ web/src/components/Error.js
@@ -1,10 +1,21 @@
 import React from 'react'
 
+function improveMessage(message) {
+  if (/ 400/.test(message)) {
+    return 'Please check the entered values'
+  }
+  else if (/ 401/.test(message)) {
+    return 'You must be signed in'
+  }
+
+  return message
+}
+
 function Error({
   error
 }) {
   return (
-    <p>{ error.message }</p>
+    <p>{ improveMessage(error.message) }</p>
   )
 }
 
